d3fa95b838ece81feb3e4e9ed625c78fd1250c7d
Shoji Takayuki <tosho427.sysdiv@gmail.com>
2024-02-04 03:35:36 +0900

    プロジェクト作成時の画面をファイル分割して作成

---
+++ lib/page/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_develop_app/page/sample_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutterサンプル画面HOME'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SamplePage(
+                          title: '初回サンプルページ',
+                        )),
+              );
+            },
+            child: const Text('プロジェクト作成時サンプル画面'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('webviewPage'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('無限スクロールPage'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('ログインPage'),
+          ),
+        ],
+      ),
+    );
+  }
+}
