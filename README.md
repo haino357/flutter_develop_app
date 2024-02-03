e64db367f45bb77f5c043502afb5db77682e06ec
Shoji Takayuki <tosho427.sysdiv@gmail.com>
2024-02-03 18:48:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flutter_develop_app
+技術検証のために作成したFlutterアプリ
+下記に使用した技術を記載する。
+
---
+++ android/app/src/main/kotlin/com/flutter_develop_app/flutter_develop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_develop_app.flutter_develop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
