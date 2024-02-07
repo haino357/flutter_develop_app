dd865e7a5a95c44cbc30feb0ed94de1afc2968d4
Shoji Takayuki <tosho427.sysdiv@gmail.com>
2024-02-07 23:55:42 +0900

    アコーディオンViewを追加

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Item> data = generateItems(5);
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -45,8 +47,42 @@ class HomePage extends StatelessWidget {
                 },
                 child: const Text('ログインPage'),
               ),
+              // アコーディオンView
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ExpansionTile(
+                    title: Text(data[index].headerValue),
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(data[index].expandedValue),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ],
           ),
         ));
   }
+}
+
+class Item {
+  Item({
+    required this.expandedValue,
+    required this.headerValue,
+  });
+
+  String expandedValue;
+  String headerValue;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List<Item>.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'アイテム $index',
+      expandedValue: 'このアイテムの詳細情報',
+    );
+  });
 }
