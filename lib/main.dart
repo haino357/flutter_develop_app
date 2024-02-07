f50dc78e23381bbc9271895a4abc620382b7dd1e
Shoji Takayuki <tosho427.sysdiv@gmail.com>
2024-02-07 23:54:08 +0900

    ページルーティングを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_develop_app/page/home_page.dart';
+import 'package:flutter_develop_app/page/login_page.dart';
+import 'package:flutter_develop_app/page/sample_page.dart';
+import 'package:flutter_develop_app/page/webview_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // デバッグバナーを非表示
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
+        debugShowCheckedModeBanner: false, // デバッグバナーを非表示
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
+        // 画面遷移ルーティングを記載する
+        routes: <String, WidgetBuilder>{
+          '/home': (BuildContext context) => const HomePage(),
+          '/sample': (BuildContext context) => const SamplePage(
+                title: '初回サンプルページ',
+              ),
+          '/login': (BuildContext context) => const LoginPage(),
+          '/webview': (BuildContext context) => const WebviewPage(),
+        });
   }
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_develop_app/page/login_page.dart';
-import 'package:flutter_develop_app/page/sample_page.dart';
-import 'package:flutter_develop_app/page/webview_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,49 +6,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutterサンプル画面HOME'),
-      ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const SamplePage(
-                          title: '初回サンプルページ',
-                        )),
-              );
-            },
-            child: const Text('プロジェクト作成時サンプル画面'),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Flutterサンプル画面HOME'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/sample');
+                },
+                child: const Text('プロジェクト作成時サンプル画面'),
+              ),
+              // タイトル
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/webview',
+                  );
+                },
+                child: const Text('webviewPage'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('無限スクロールPage'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const LoginPage()),
+                  // );
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: const Text('ログインPage'),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const WebviewPage()),
-              );
-            },
-            child: const Text('webviewPage'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('無限スクロールPage'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginPage()),
-              );
-            },
-            child: const Text('ログインPage'),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
